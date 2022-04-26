58b7a6febe5e85bccb66697fe4a81b98f209bb7a
likakuli <1154584512@qq.com>
2022-04-26 21:43:49 +0800

    feat: support transport middleware (#1)
    * feat: support policy transport
    
    Signed-off-by: kaku <1154584512@qq.com>

---
+++ Makefile
@@ -0,0 +1,68 @@
+GO ?= go
+GOFMT ?= gofmt "-s"
+PACKAGES ?= $(shell $(GO) list ./...)
+VETPACKAGES ?= $(shell $(GO) list ./... | grep -v /examples/)
+GOFILES := $(shell find . -name "*.go" | grep -v vendor)
+TESTFOLDER := $(shell $(GO) list ./... | grep -v examples)
+TESTTAGS ?= ""
+
+# Setting SHELL to bash allows bash commands to be executed by recipes.
+# This is a requirement for 'setup-envtest.sh' in the test target.
+# Options are set to exit when a recipe line exits non-zero or a piped command fails.
+SHELL = /usr/bin/env bash -o pipefail
+.SHELLFLAGS = -ec
+
+##@ General
+
+# The help target prints out all targets with their descriptions organized
+# beneath their categories. The categories are represented by '##@' and the
+# target descriptions by '##'. The awk commands is responsible for reading the
+# entire set of makefiles included in this invocation, looking for lines of the
+# file as xyz: ## something, and then pretty-format the target and help. Then,
+# if there's a line with ##@ something, that gets pretty-printed as a category.
+# More info on the usage of ANSI control characters for terminal formatting:
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
+# More info on the awk command:
+# http://linuxcommand.org/lc3_adv_awk.php
+
+.PHONY: help
+help: ## Display this help.
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+.PHONY: test
+test: ## Run project unit test and generate coverage result
+	echo "mode: count" > coverage.out
+	for d in $(TESTFOLDER); do \
+		$(GO) test -tags $(TESTTAGS) -v -covermode=count -coverprofile=profile.out $$d > tmp.out; \
+		cat tmp.out; \
+		if grep -q "^--- FAIL" tmp.out; then \
+			rm tmp.out; \
+			exit 1; \
+		elif grep -q "build failed" tmp.out; then \
+			rm tmp.out; \
+			exit 1; \
+		elif grep -q "setup failed" tmp.out; then \
+			rm tmp.out; \
+			exit 1; \
+		fi; \
+		if [ -f profile.out ]; then \
+			cat profile.out | grep -v "mode:" >> coverage.out; \
+			rm profile.out; \
+		fi; \
+	done
+
+.PHONY: fmt
+fmt: ## Format project files
+	@$(GOFMT) -w $(GOFILES)
+
+.PHONY: fmt-check
+fmt-check: ## Check project files format info
+	@diff=$$($(GOFMT) -d $(GOFILES)); \
+	if [ -n "$$diff" ]; then \
+		echo "Please run 'make fmt' and commit the result:"; \
+		echo "$${diff}"; \
+		exit 1; \
+	fi;
+
+vet: ## Vet project files
+	@$(GO) vet $(VETPACKAGES)
---
+++ transport_test.go
@@ -0,0 +1,129 @@
+package pidalio
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/k-cloud-labs/pkg/util/overridemanager"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+
+	policyv1alpha1 "github.com/k-cloud-labs/pkg/apis/policy/v1alpha1"
+	v1alpha10 "github.com/k-cloud-labs/pkg/client/listers/policy/v1alpha1"
+	"github.com/k-cloud-labs/pkg/test/helper"
+	"github.com/k-cloud-labs/pkg/test/mock"
+	"github.com/k-cloud-labs/pkg/util"
+	utilhelper "github.com/k-cloud-labs/pkg/util/helper"
+)
+
+func TestPolicyTransport_RoundTrip(t *testing.T) {
+	deployment := helper.NewDeployment(metav1.NamespaceDefault, "test")
+	deploymentObj, _ := utilhelper.ToUnstructured(deployment)
+
+	overriders1 := policyv1alpha1.Overriders{
+		Plaintext: []policyv1alpha1.PlaintextOverrider{
+			{
+				Path:     "/metadata/annotations",
+				Operator: "add",
+				Value:    apiextensionsv1.JSON{Raw: []byte("{\"foo\": \"bar\"}")},
+			},
+		},
+	}
+	overriders2 := policyv1alpha1.Overriders{
+		Plaintext: []policyv1alpha1.PlaintextOverrider{
+			{
+				Path:     "/metadata/annotations",
+				Operator: "add",
+				Value:    apiextensionsv1.JSON{Raw: []byte("{\"hello\": \"world\"}")},
+			},
+		},
+	}
+
+	overridePolicy1 := &policyv1alpha1.OverridePolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: metav1.NamespaceDefault,
+			Name:      "overridePolicy1",
+		},
+		Spec: policyv1alpha1.OverridePolicySpec{
+			ResourceSelectors: []policyv1alpha1.ResourceSelector{
+				{
+					APIVersion: deployment.APIVersion,
+					Kind:       deployment.Kind,
+					Name:       deployment.Name,
+				},
+			},
+			OverrideRules: []policyv1alpha1.RuleWithOperation{
+				{
+					TargetOperations: []string{util.Create},
+					Overriders:       overriders1,
+				},
+			},
+		},
+	}
+	overridePolicy2 := &policyv1alpha1.ClusterOverridePolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "overridePolicy2",
+		},
+		Spec: policyv1alpha1.OverridePolicySpec{
+			OverrideRules: []policyv1alpha1.RuleWithOperation{
+				{
+					TargetOperations: []string{util.Create, util.Update},
+					Overriders:       overriders2,
+				},
+			},
+		},
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	copLister := mock.NewMockClusterOverridePolicyLister(ctrl)
+	opLister := mock.NewMockOverridePolicyLister(ctrl)
+
+	opLister.EXPECT().List(labels.Everything()).Return([]*policyv1alpha1.OverridePolicy{
+		overridePolicy1,
+	}, nil)
+
+	copLister.EXPECT().List(labels.Everything()).Return([]*policyv1alpha1.ClusterOverridePolicy{
+		overridePolicy2,
+	}, nil)
+
+	manager := overridemanager.NewOverrideManager(copLister, opLister)
+
+	tests := []struct {
+		name              string
+		opLister          v1alpha10.OverridePolicyLister
+		copLister         v1alpha10.ClusterOverridePolicyLister
+		resource          *unstructured.Unstructured
+		operation         string
+		wantedAnnotations map[string]string
+		wantedErr         error
+	}{
+		{
+			name:      "OverrideRules test 1",
+			opLister:  opLister,
+			copLister: copLister,
+			resource:  deploymentObj,
+			operation: util.Create,
+			wantedErr: nil,
+			wantedAnnotations: map[string]string{
+				"foo":                        "bar",
+				util.AppliedOverrides:        `[{"policyName":"overridePolicy1","overriders":{"plaintext":[{"path":"/metadata/annotations","operator":"add","value":{"foo":"bar"}}]}}]`,
+				util.AppliedClusterOverrides: `[{"policyName":"overridePolicy2","overriders":{"plaintext":[{"path":"/metadata/annotations","operator":"add","value":{"hello":"world"}}]}}]`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := ApplyOverridePolicy(manager, tt.resource, tt.operation); !reflect.DeepEqual(err, tt.wantedErr) {
+				t.Errorf("ApplyOverridePolicy() = %v, want %v", err, tt.wantedErr)
+			}
+			if !reflect.DeepEqual(deploymentObj.GetAnnotations(), tt.wantedAnnotations) {
+				t.Errorf("ApplyOverridePolicy() Annotation = %v, want %v", deploymentObj.GetAnnotations(), tt.wantedAnnotations)
+			}
+		})
+	}
+}
